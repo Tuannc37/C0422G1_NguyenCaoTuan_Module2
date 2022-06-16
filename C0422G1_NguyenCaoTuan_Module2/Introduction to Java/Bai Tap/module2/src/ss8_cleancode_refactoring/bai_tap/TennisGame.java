fce109ea0ad3bf85b3894f3c54098478b4dbc221
caotuan <caotuan1994hk@gmail.com>
2022-06-16 19:30:11 +0700

    update

--- C0422G1_NguyenCaoTuan_Module2/Introduction to Java/Bai Tap/module2/src/ss8_cleancode_refactoring/bai_tap/TennisGame.java
+++ C0422G1_NguyenCaoTuan_Module2/Introduction to Java/Bai Tap/module2/src/ss8_cleancode_refactoring/bai_tap/TennisGame.java
@@ -6,7 +6,7 @@ public class TennisGame {
     public static final String POINTS_NAME_THIRTY = "Thirty";
     public static final String POINTS_NAME_FORTY = "Forty";
     public static final String POINTS_NAME_DEUCE = "Deuce";
-    public static final String POINTS_NAME_ALL = "All";
+    public static final String POINTS_NAME_ALL = "- All";
 
     public static final int POINTS_VALUE_LOVE = 0;
     public static final int POINTS_VALUE_FIFTEEN = 1;
@@ -35,9 +35,9 @@ public class TennisGame {
         return score;
     }
 
-    public static String advantagePlayer(int scorePlayer1, int scorePlayer2) {
+    public static String advantagePlayer(int scorePlayerFirst, int scorePlayerSecond) {
         String score;
-        int minusScore = scorePlayer1 - scorePlayer2;
+        int minusScore = scorePlayerFirst - scorePlayerSecond;
         if (minusScore == 1) score = "Advantage player1";
         else if (minusScore == -1) score = "Advantage player2";
         else if (minusScore >= 2) score = "Win for player1";
@@ -45,14 +45,14 @@ public class TennisGame {
         return score;
     }
 
-    public static String underDeuce(int scorePlayer1, int scorePlayer2) {
+    public static String underDeuce(int scorePlayerFirst, int scorePlayerSecond) {
         int tempScore;
         StringBuilder score = new StringBuilder();
         for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = scorePlayer1;
+            if (i == 1) tempScore = scorePlayerFirst;
             else {
                 score.append("-");
-                tempScore = scorePlayer2;
+                tempScore = scorePlayerSecond;
             }
             switch (tempScore) {
                 case POINTS_VALUE_LOVE:
@@ -72,14 +72,14 @@ public class TennisGame {
         return score.toString();
     }
 
-    public static String getScore(int scorePlayer1, int scorePlayer2) {
+    public static String getScore(int scorePlayerFirst, int scorePlayerSecond) {
         String score;
-        if (scorePlayer1 == scorePlayer2) {
-            score = tieScore(scorePlayer1);
-        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
-            score = advantagePlayer(scorePlayer1, scorePlayer2);
+        if (scorePlayerFirst == scorePlayerSecond) {
+            score = tieScore(scorePlayerFirst);
+        } else if (scorePlayerFirst >= 4 || scorePlayerSecond >= 4) {
+            score = advantagePlayer(scorePlayerFirst, scorePlayerSecond);
         } else {
-            score = underDeuce(scorePlayer1, scorePlayer2);
+            score = underDeuce(scorePlayerFirst, scorePlayerSecond);
         }
         return score;
     }
